1b4a45057d0fef57cda94a12936be0b90a63fbf8
yuiio <antony.templier@gmail.com>
2019-04-15 15:39:48 +0200

    Refactor : dekamouloxage

--- entities.py
+++ entities.py
@@ -1,5 +1,6 @@
 import pyxel as px
 
+
 class Entities:
     def __init__(self, x, y, h, w, col):
         self.x = x
@@ -7,22 +8,41 @@ class Entities:
         self.h = h
         self.w = w
         self.col = col
-        self.list_entity = []
-        
+        # self.list_entity = []
+
     def update(self):
         pass
 
     def draw(self):
-        self.list_entity.append([self.x, self.y, self.h, self.w, self.col])   
-        for entity in self.list_entity:
-            px.rect(*entity)
+        # self.list_entity.append([self.x, self.y, self.h, self.w, self.col])
+        # for entity in self.list_entity:
+        #     px.rect(*entity)
+        pass
+
 
 class Bullet(Entities):
     pass
 
+
 class Spacecraft(Entities):
+    def __init__(self, x, y, h, w, col):
+        self.speed = 3
+        super().__init__(x, y, h, w, col)
+
+    def update(self):
+        if px.btnp(px.KEY_UP, 1, 1):
+            self.y -= self.speed
+        if px.btnp(px.KEY_DOWN, 1, 1):
+            self.y += self.speed
+        if px.btnp(px.KEY_LEFT, 1, 1):
+            self.x -= self.speed
+        if px.btnp(px.KEY_RIGHT, 1, 1):
+            self.x += self.speed
+
+    def draw(self):
+        px.rect(self.x, self.y, self.x + self.h, self.y + self.w, self.col)
 
     def shoot(self):
         if px.btnp(px.KEY_SPACE, 1, 1):
-            print('Shoot = True')
+            print("Shoot = True")
             # Spacecraft if px.btnp(px.KEY_SPACE, 10, 10): Bullet : px.rect(self.x + 7, self.y - 2, self.x + 8, self.y - 1, 7)
--- main.py
+++ main.py
@@ -4,41 +4,22 @@ from entities import Entities, Spacecraft, Bullet
 WIDTH = 150
 LENGTH = 200
 
+
 class App:
     def __init__(self):
         px.init(WIDTH, LENGTH)
-        self.x = WIDTH // 2
-        self.y = LENGTH // 2
-        self.shoot = False
+        self.player = Spacecraft(WIDTH // 2, LENGTH // 2, 15, 15, 4)
         px.run(self.update, self.draw)
 
     def update(self):
-
-        def update_player():
-            p_speed = 3
-            if px.btnp(px.KEY_UP, 1, 1):
-                self.y -= p_speed
-            if px.btnp(px.KEY_DOWN, 1, 1):
-                self.y += p_speed
-            if px.btnp(px.KEY_LEFT, 1, 1):
-                self.x -= p_speed
-            if px.btnp(px.KEY_RIGHT, 1, 1):
-                self.x += p_speed
-        update_player()
-        
-        # Activate the Entity Class, Entity class only activate a draw rect for now
-        self.player = Spacecraft(self.x, self.y, self.x + 15, self.y + 15, 4)
-        self.bullet = Bullet(self.x + 7, self.y - 2, self.x + 8, self.y - 1, 5)
-        self.bullet.update()
+        self.player.update()
 
     def draw(self):
         px.cls(0)
         self.player.draw()
-        self.bullet.draw()
-        
-        # px.rect(self.x + 7, self.y - 2, self.x + 8, self.y - 1, 7)
-        
+
         # Test line
         px.line(55, 23, 65, 90, 12)
+
 
 App()
