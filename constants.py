75b422e8ae032edbd47e906a1ac7fc6107390694
Seubmarine <seubmarinepro@gmail.com>
2019-05-24 20:05:28 +0200

    New colors and better descriptions in constants

--- constants.py
+++ constants.py
@@ -1,12 +1,22 @@
 """All of the game constant"""
 
+# Set Frame per seconds
+FPS = 60
+
+# dimension
 WIDTH = 100
 HEIGHT = 150
 
+# colors
+BLACK = 0
+BLUE = 1
+PURPLE = 2
+DARK_GREEN = 3
+BROWN = 4
+GREY = 5
 WHITE = 7
 RED = 8
+ORANGE = 9
 YELLOW = 10
 GREEN = 11
-BLUE = 12
-
-FPS = 60
+CYAN = 12
